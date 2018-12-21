8d32a73494a5b6bd35587fab685446f1d95e9707
Ben Lithgow Smith <benlithgow@gmail.com>
2018-12-21 18:25:08 +0000

    replace conditional with polymorphism, NewReleasePrice.getCharge

--- src/main/java/org/example/refactoring/Price.java
+++ src/main/java/org/example/refactoring/Price.java
@@ -41,6 +41,10 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    double getCharge(int daysRented){
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
